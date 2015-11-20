a073f4155120a83c0438add7c9c706d7890dda5b
CCHS RoboChargers <cchsrobochargers@gmail.com>
2015-11-20 15:10:29 -0500

    worked on 5256 autonomous

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/CCHS5256Autonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/CCHS5256Autonomous.java
@@ -1,7 +1,272 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import java.util.Date;
 
 /**
  * Created by Robotics on 10/7/2015.
  */
-public class CCHS5256Autonomous {
+public class CCHS5256Autonomous extends OpMode {
+    enum MoveState {
+        DELAY, STARTMOVE, MOVING, START1, START2, START3, START4, START5, DELAY1, DELAY2, DELAY3, DELAY4, DONE
+    }
+
+    enum BeaconState {
+        NOT_LOOKING, LOOKING_START, LOOKING_END, DONE
+    }
+
+//    enum BeaconColor {
+//        RED, BLUE
+//    }
+//
+//    enum RobotSide {
+//        LEFT, RIGHT
+//    }
+
+    DcMotorController driveTrainController;
+    DcMotor motorRight;
+    DcMotor motorLeft;
+    ColorSensor ColorSense;
+    //GyroSensor Gyro;
+    //IrSeekerSensor IrSense;
+    OpticalDistanceSensor OpticalDistance;
+    MoveState currentMove;
+    MoveState nextMove;
+    BeaconState redState;
+    //double[][][] beaconLocation;
+    int redStartRight;
+    int redStartLeft;
+    int redEndRight;
+    int redEndLeft;
+    BeaconState blueState;
+    int blueStartRight;
+    int blueStartLeft;
+    int blueEndRight;
+    int blueEndLeft;
+    long delayUntil;
+    Date now;
+
+    public TestAutonomous() {
+    }
+
+    int centimetersToCounts(double centimeters) {
+        double wheelDiameter = 10.1;    // wheel diameter in cm
+        double encoderCounts = 1120.0;  // encoder counts per revolution of the drive train motors
+        return (int) ((centimeters / (wheelDiameter * Math.PI)) * encoderCounts);
+    }
+
+    double countsToCentimeters(int counts) {
+        double wheelDiameter = 10.1;
+        double encoderCounts = 1120.0;
+        return (((double)counts / encoderCounts) * (wheelDiameter * Math.PI));
+    }
+
+    int degreesToCounts(double degrees) {
+        double wheelBase = 40.3225; // wheelbase of the primary drive wheels
+        double oneDegree = ((wheelBase * Math.PI) / 360);   // calculates the distance of one degree
+        return centimetersToCounts(oneDegree * degrees);
+    }
+
+    void moveStraight(double distanceCM, double speed) {
+        int rightTarget;
+        int leftTarget;
+
+        leftTarget = motorLeft.getCurrentPosition() + centimetersToCounts(distanceCM);
+        motorLeft.setTargetPosition(leftTarget);
+        rightTarget = motorRight.getCurrentPosition() + centimetersToCounts(distanceCM);
+        motorRight.setTargetPosition(rightTarget);
+        motorLeft.setPower(speed);
+        motorRight.setPower(speed);
+    }
+
+    /**
+     * if degree magnitude is negative, robot turns clockwise
+     *
+     * @param degrees
+     * @param speed
+     */
+    void moveTurn(double degrees, double speed) {
+        int rightTarget;
+        int leftTarget;
+
+        leftTarget = motorLeft.getCurrentPosition() - degreesToCounts(degrees);
+        motorLeft.setTargetPosition(leftTarget);
+        rightTarget = motorRight.getCurrentPosition() + degreesToCounts(degrees);
+        motorRight.setTargetPosition(rightTarget);
+        motorLeft.setPower(speed);
+        motorRight.setPower(speed);
+    }
+
+    @Override
+    public void init() {
+        driveTrainController = hardwareMap.dcMotorController.get("dtCtlr");
+        motorRight = hardwareMap.dcMotor.get("motorR");
+        motorLeft = hardwareMap.dcMotor.get("motorL");
+        ColorSense = hardwareMap.colorSensor.get("color");
+        ColorSense.enableLed(true);
+        //Gyro = hardwareMap.gyroSensor.get("GyroSense");
+        OpticalDistance = hardwareMap.opticalDistanceSensor.get("opDistance");
+        //IrSense = hardwareMap.irSeekerSensor.get("IRSense");
+        motorRight.setDirection(DcMotor.Direction.REVERSE);
+        motorRight.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        motorLeft.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        currentMove = MoveState.START1;
+        redState = BeaconState.NOT_LOOKING;
+        blueState = BeaconState.NOT_LOOKING;
+        //beaconLocation = new double[2][2][2];
+    }
+
+    @Override
+    public void loop() {
+
+        switch (currentMove) {
+            case DELAY:
+                now = new Date();
+                if (now.getTime() >= delayUntil) {
+                    currentMove = nextMove;
+                }
+                break;
+
+            case STARTMOVE:
+                if (motorLeft.isBusy() && motorRight.isBusy()) {
+                    currentMove = MoveState.MOVING;
+                }
+                break;
+
+            case MOVING:
+                if (ColorSense.red() >= 1) {
+                    motorRight.setPower(0.0);
+                    motorLeft.setPower(0.0);
+                    currentMove = nextMove;
+                }
+                if (!motorLeft.isBusy() && !motorRight.isBusy()) {
+                    currentMove = nextMove;
+                }
+                break;
+
+            case START1:
+                moveStraight(85.0, 0.5);
+                currentMove = MoveState.STARTMOVE;
+                nextMove = MoveState.DELAY1;
+                break;
+
+            case DELAY1:
+                now = new Date();
+                delayUntil = now.getTime() + 100;
+                currentMove = MoveState.DELAY;
+                nextMove = MoveState.START2;
+                break;
+
+            case START2:
+                moveTurn(45.0, 0.5);
+                currentMove = MoveState.STARTMOVE;
+                nextMove = MoveState.DELAY2;
+                break;
+
+            case DELAY2:
+                now = new Date();
+                delayUntil = now.getTime() + 100;
+                currentMove = MoveState.DELAY;
+                nextMove = MoveState.START3;
+                break;
+
+            case START3:
+                moveStraight(266.7, 0.5);
+                currentMove = MoveState.STARTMOVE;
+                nextMove = MoveState.DELAY3;
+                break;
+
+            case DELAY3:
+                now = new Date();
+                delayUntil = now.getTime() + 100;
+                currentMove = MoveState.DELAY;
+                nextMove = MoveState.START4;
+                break;
+
+            case START4:
+                moveTurn(-45.0, 0.5);
+                currentMove = MoveState.STARTMOVE;
+                nextMove = MoveState.DELAY4;
+                break;
+
+            case DELAY4:
+                now = new Date();
+                delayUntil = now.getTime() + 100;
+                currentMove = MoveState.DELAY;
+                nextMove = MoveState.START5;
+                break;
+
+            case START5:
+                moveStraight(-91.4, 0.5);
+                currentMove = MoveState.STARTMOVE;
+                nextMove = MoveState.DONE;
+                break;
+
+            case DONE:
+                motorLeft.setPower(0.0);
+                motorRight.setPower(0.0);
+                break;
+        }
+        switch (redState) {
+            case NOT_LOOKING:
+                break;
+            case LOOKING_START:
+                if (ColorSense.red() >= 1) {
+                    redStartLeft = motorLeft.getCurrentPosition();
+                    redStartRight = motorRight.getCurrentPosition();
+                    redState = BeaconState.LOOKING_END;
+                }
+                break;
+            case LOOKING_END:
+                if (ColorSense.red() == 0) {
+                    redEndLeft = motorLeft.getCurrentPosition();
+                    redEndRight = motorRight.getCurrentPosition();
+                    redState = BeaconState.DONE;
+                }
+                break;
+            case DONE:
+                break;
+        }
+        switch (blueState) {
+            case NOT_LOOKING:
+                break;
+            case LOOKING_START:
+                if (ColorSense.blue() >= 1) {
+                    blueStartLeft = motorLeft.getCurrentPosition();
+                    blueStartRight = motorRight.getCurrentPosition();
+                    blueState = BeaconState.LOOKING_END;
+                }
+                break;
+            case LOOKING_END:
+                if (ColorSense.blue() == 0) {
+                    blueEndLeft = motorLeft.getCurrentPosition();
+                    blueEndRight = motorRight.getCurrentPosition();
+                    blueState = BeaconState.DONE;
+                }
+                break;
+            case DONE:
+                break;
+        }
+        if (gamepad1.start) {
+            currentMove = MoveState.START1;
+        }
+
+        telemetry.addData("Text", "*** Robot Data***");
+        telemetry.addData("Text", "Look for Red");
+        telemetry.addData("RedStart", redStartLeft);
+        telemetry.addData("RedEnd", redEndLeft);
+        telemetry.addData("BlueStart", blueStartLeft);
+        telemetry.addData("BlueEnd", blueEndLeft);
+        //telemetry.addData("GyroSense", (float) Gyro.getRotation());
+        telemetry.addData("OpticalSense", (float) OpticalDistance.getLightDetectedRaw());
+        telemetry.addData("ENCLeft", (float) motorLeft.getCurrentPosition());
+        telemetry.addData("ENCRight", (float) motorRight.getCurrentPosition());
+    }
+
+    @Override
+    public void stop() {
+    }
 }
