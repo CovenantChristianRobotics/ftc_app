439f18b70e41cecef931c08be0d423bf1eb05982
CCHSRoboChargers <cchsrobochargers@gmail.com>
2015-12-12 15:49:33 -0500

    4507 trackbot configuration with teleop
    Right/left drive is flipped, need to run with encoders, etc. but it has basic functionality.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/CCHS4507TeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/CCHS4507TeleOp.java
@@ -7,32 +7,48 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.ServoController;
 import com.qualcomm.robotcore.hardware.UltrasonicSensor;
-import com.qualcomm.robotcore.util.Range;
-
-import java.util.Date;
 
 /**
  * Created by cchsrobochargers on 12/10/15.
  */
 public class CCHS4507TeleOp extends OpMode {
 
-    DcMotorController driveTrainController;
-    DcMotorController auxMotorController;
     DcMotor motorRight;
     DcMotor motorLeft;
     DcMotor trackLifter;
+    //servos
+    Servo servoBeaconPinion;
+    Servo servoBeaconPusher;
+    Servo servoClimberDumper;
+    Servo servoDist;
+    ColorSensor ColorSense;
+    GyroSensor gyroSense;
+    UltrasonicSensor ultraSense;
+    //switches
+    DigitalChannel nearMountainSwitch;
+    DigitalChannel redBlueSwitch;
+    boolean nearMountainFlag = false;
+    boolean redAllianceFlag = false;
+
+
 
     @Override
     public void init() {
         //Ints hardware
-        driveTrainController = hardwareMap.dcMotorController.get("dtCtlr");
-        auxMotorController = hardwareMap.dcMotorController.get("auxCtlr");
         motorRight = hardwareMap.dcMotor.get("motorR");
         motorLeft = hardwareMap.dcMotor.get("motorL");
         trackLifter = hardwareMap.dcMotor.get("trkLftr");
-
+        //servos
+        servoBeaconPusher = hardwareMap.servo.get("beacon_pusher");
+        servoClimberDumper = hardwareMap.servo.get("climber_dumper");
+        servoDist = hardwareMap.servo.get("servoDist");
+        ColorSense = hardwareMap.colorSensor.get("color");
+        nearMountainSwitch = hardwareMap.digitalChannel.get("nearMtnSw");
+        redBlueSwitch = hardwareMap.digitalChannel.get("rbSw");
+        ultraSense = hardwareMap.ultrasonicSensor.get("ultraSense");
+        gyroSense = hardwareMap.gyroSensor.get("gyro");
+        nearMountainFlag = nearMountainSwitch.getState();
         //set up motors
         motorRight.setDirection(DcMotor.Direction.REVERSE);
         motorLeft.setDirection(DcMotor.Direction.FORWARD);
@@ -40,6 +56,7 @@ public class CCHS4507TeleOp extends OpMode {
         motorRight.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
         motorLeft.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
         trackLifter.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+
     }
 
     @Override
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -83,7 +83,7 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("TurnTeleOp", TurnTeleOp.class);
     manager.register("TestAutonomous", TestAutonomous.class);
     manager.register("SpareParts5256Autonomous", SpareParts5256Autonomous.class);
-    manager.register("CHS4507TeleOp", CCHS4507TeleOp.class);
+    manager.register("CCHS4507TeleOp", CCHS4507TeleOp.class);
 
     //manager.register("NxtTeleOp", NxtTeleOp.class);
 
