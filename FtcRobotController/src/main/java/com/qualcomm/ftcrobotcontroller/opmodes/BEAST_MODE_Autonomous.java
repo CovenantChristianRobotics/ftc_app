3a8b5397f989756091672441b8454434c9583d0b
CCHS RoboChargers <cchsrobochargers@gmail.com>
2016-02-25 13:17:42 -0500

    added movestates

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BEAST_MODE_Autonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BEAST_MODE_Autonomous.java
@@ -28,10 +28,10 @@ public class BEAST_MODE_Autonomous extends OpMode {
     //movestate for our autonomous move
     enum MoveState {
         STARTMOVE, MOVINGSTRAIGHT, STARTTURN, MOVINGTURN, DELAYSETTINGS, DELAY,
-        INITIALIZEROBOT, CHOOSEMOVE, FIRSTMOVE, TURNDIAG, MOVEDIAG, TURNONCOLOREDLINE, GOTOWHITELINE,
-        TURNTOBEACON, DRIVETOBEACON, READBEACON, PUSHBUTTON, UNPUSHBUTTON, BACKUP,
-        EXTENDARM, DUMPCLIMBERS, JIGGLEFORWARD, JIGGLEBACKWARD, PULLARMIN, BACKUPFARTHER, TURNALONGLINE, DRIVEALONGLINE, 
-        TURNTOMOUNTAIN, DRIVETOMOUNTAIN, GOUPMOUNTAIN, PREPTELEOP, DONE
+        DRIVEAWAYFROMWALL, TURNDIAG, MOVEDIAG, GOPASTREDTAPE, TURNPARALLELTOWALL,
+        DRIVETOWHITELINE, TURNTOBEACON, DRIVETOBEACON, WINDOUTARM, DUMPCLIMBERS,
+        READBEACON, WINDINARMPARTWAY, TURNTOBUTTON, PUSHBUTTON, BACKUP,
+        TURNTOFLOORGOAL, DRIVEINFLOORGOAL, DONE
     }
     //movestate for our omniwheels in autonomous
     enum OmniCtlr {
@@ -47,7 +47,6 @@ public class BEAST_MODE_Autonomous extends OpMode {
     // Servos
     Servo armLock;
     Servo climberDumper;
-    Servo ultraSenseServo;
     Servo leftOmniPinion;
     Servo rightOmniPinion;
     Servo leftPlow;
@@ -59,29 +58,23 @@ public class BEAST_MODE_Autonomous extends OpMode {
     // Sensors
     GyroSensor gyroSense;
     ColorSensor fColorSense;
+    ColorSensor bColorSense;
     UltrasonicSensor ultraSense;
     // Switches
-    DigitalChannel nearMtnSwitch;
+//    DigitalChannel nearMtnSwitch;
     DigitalChannel redBlueBeaconSwitch;
-    DigitalChannel delayPotSwitch;
+//    DigitalChannel delayPotSwitch;
     DigitalChannel thirdTileSwitch;
-    AnalogInput delayPotentiometer;
+//    AnalogInput delayPotentiometer;
     //variables
-    double diagMtnDist;
-    double turnMtnDist;
-    double toMtnDist;
     double redBlue;
     double firstMoveDist;
-    double turnDiagDegrees;
     double moveDiagDist;
     double delay;
-    boolean nearMountain;
-    boolean farMountain;
     boolean redAlliance;
     boolean blueAlliance;
     boolean thirdTile;
     boolean fourthTile;
-    boolean delayRobot;
     boolean lookingWithUltraSense;
     double targetReading;
     // State Machine Settings
@@ -91,24 +84,18 @@ public class BEAST_MODE_Autonomous extends OpMode {
     OmniCtlr currentOmni;
     OmniCtlr nextOmni;
     OmniCtlr chosenOmni;
-    double lDiff = 0.08549020;
-    double rDiff = -0.11372550;
     long delayUntil;
     long moveDelayTime;
     long commonDelayTime;
     long now;
     long nowOmni;
     long moveDelaytimeOmni;
-    long nowPlow;
-    long plowDelayUntil;
     long delayUntilOmni;
     int counter;
     double fastSpeed;
     double mediumSpeed;
     double slowSpeed;
     double turnSpeed;
-    boolean lookingForRedFlag;
-    boolean lookingForBlueFlag;
     boolean lookingForWhiteLine;
     // Elapsed Time
     ElapsedTime currentTime;
@@ -241,7 +228,6 @@ public class BEAST_MODE_Autonomous extends OpMode {
         // Servos
         armLock = hardwareMap.servo.get("armLock");
         climberDumper = hardwareMap.servo.get("climber_dumper");
-        ultraSenseServo = hardwareMap.servo.get("servoUltra");
         leftOmniPinion = hardwareMap.servo.get("lOmniPinion");
         rightOmniPinion = hardwareMap.servo.get("rOmniPinion");
         leftPlow = hardwareMap.servo.get("lP");
@@ -265,75 +251,68 @@ public class BEAST_MODE_Autonomous extends OpMode {
         // Sensors
         gyroSense = hardwareMap.gyroSensor.get("gyroSense");
         fColorSense = hardwareMap.colorSensor.get("fCS");
+//        fColorSense.setI2cAddress();
+        bColorSense = hardwareMap.colorSensor.get("bCS");
+//        bColorSense.setI2cAddress();
         ultraSense = hardwareMap.ultrasonicSensor.get("ultraSense");
         // Switches
-        nearMtnSwitch = hardwareMap.digitalChannel.get("nMtnSw");
+//        nearMtnSwitch = hardwareMap.digitalChannel.get("nMtnSw");
         redBlueBeaconSwitch = hardwareMap.digitalChannel.get("rBSw");
-        delayPotSwitch = hardwareMap.digitalChannel.get("dPotSw");
+//        delayPotSwitch = hardwareMap.digitalChannel.get("dPotSw");
         thirdTileSwitch = hardwareMap.digitalChannel.get("tileSw");
-        delayPotentiometer = hardwareMap.analogInput.get("delPot");
+//        delayPotentiometer = hardwareMap.analogInput.get("delPot");
         //statemachine settings
-        currentMove = MoveState.INITIALIZEROBOT;
-        nextMove = MoveState.FIRSTMOVE;
-        telemetryMove = MoveState.FIRSTMOVE;
+        currentMove = MoveState.DRIVEAWAYFROMWALL;
+        telemetryMove = MoveState.DRIVEAWAYFROMWALL;
         currentOmni = OmniCtlr.NOTMOVING;
         chosenOmni = OmniCtlr.NOTMOVING;
         counter = 0;
         // Set Switch Flags
         if (redBlueBeaconSwitch.getState()) {   // WE ARE BLUE
             redBlue = -1.0;
-            ultraSenseServo.setPosition(0.75);
             redAlliance = false;
             blueAlliance = true;
         } else {                                // WE ARE RED
             redBlue = 1.0;
-            ultraSenseServo.setPosition(0.25);
             redAlliance = true;
             blueAlliance = false;
         }
 
-        if (nearMtnSwitch.getState()) {         // WE GO TO NEAR MOUNTAIN
-            diagMtnDist = 0.0;
-            turnMtnDist = -90.0;
-            toMtnDist = 100.0;
-            nearMountain = true;
-            farMountain = false;
-        } else {                                // WE GO TO FAR MOUNTAIN
-            diagMtnDist = 50.0;
-            turnMtnDist = 90.0;
-            toMtnDist = 200.0;
-            nearMountain = false;
-            farMountain = true;
-        }
-
-        if (delayPotSwitch.getState()) {        // WE DELAY
-            delay = (delayPotentiometer.getValue() * (10000 / 1024));
-            delayRobot = true;
-        } else {                                // WE DON'T DELAY
-            delay = 0.0;
-            delayRobot = false;
-        }
-        delay = 0.0;
-        delayRobot = false;
+//        if (nearMtnSwitch.getState()) {         // WE GO TO NEAR MOUNTAIN
+//            diagMtnDist = 0.0;
+//            turnMtnDist = -90.0;
+//            toMtnDist = 100.0;
+//            nearMountain = true;
+//            farMountain = false;
+//        } else {                                // WE GO TO FAR MOUNTAIN
+//            diagMtnDist = 50.0;
+//            turnMtnDist = 90.0;
+//            toMtnDist = 200.0;
+//            nearMountain = false;
+//            farMountain = true;
+//        }
+//
+//        if (delayPotSwitch.getState()) {        // WE DELAY
+//            delay = (delayPotentiometer.getValue() * (10000 / 1024));
+//            delayRobot = true;
+//        } else {                                // WE DON'T DELAY
+//            delay = 0.0;
+//            delayRobot = false;
+//        }
+//        delay = 0.0;
+//        delayRobot = false;
 
         if (thirdTileSwitch.getState()) {       // WE ARE ON THE THIRD TILE FROM THE MOUNTAIN CORNER
-            firstMoveDist = 71.44;
-            turnDiagDegrees = 45.0;
-            moveDiagDist = 0.0;
+            firstMoveDist = 6.35;
+            moveDiagDist = 228.6;
             thirdTile = true;
             fourthTile = false;
         } else {                                // WE ARE ON THE FOURTH TILE FROM THE MOUNTAIN CORNER
-            firstMoveDist = 0.0;
-            turnDiagDegrees = 0.0;
-            moveDiagDist = 30.38;
+            firstMoveDist = 67.31;
+            moveDiagDist = 139.7;
             thirdTile = false;
             fourthTile = true;
         }
-        firstMoveDist = 71.44;
-        moveDiagDist = 0.0;
-        thirdTile = true;
-        fourthTile = false;
-
         // State Machine Settings
         fastSpeed = 0.8;
         mediumSpeed = 0.5;
@@ -457,37 +436,16 @@ public class BEAST_MODE_Autonomous extends OpMode {
                 break;
 
             // MOVES WE USE ONCE, IN A SEQUENCE
-            case INITIALIZEROBOT:
-                currentTime.reset();
-                moveDelayTime = (long)delay;
-                currentMove = MoveState.DELAYSETTINGS;
-                nextMove = MoveState.FIRSTMOVE;
-                telemetryMove = MoveState.INITIALIZEROBOT;
-                moveDelayTime = commonDelayTime;
-                break;
-
-//            case CHOOSEMOVE:
-//                if (thirdTile) {
-//                    currentMove = MoveState.FIRSTMOVE;
-//                } else if (fourthTile){
-//                    currentMove = MoveState.MOVEDIAG;
-//                }
-//                telemetryMove = MoveState.CHOOSEMOVE;
-//                moveDelayTime = commonDelayTime;
-//                break;
-
-            case FIRSTMOVE:
-                // Move Straight firstMoveDist
-                moveStraight(firstMoveDist, mediumSpeed);
+            case DRIVEAWAYFROMWALL:
+                moveStraight(firstMoveDist, fastSpeed);
                 currentMove = MoveState.STARTMOVE;
                 nextMove = MoveState.TURNDIAG;
-                telemetryMove = MoveState.FIRSTMOVE;
+                telemetryMove = MoveState.DRIVEAWAYFROMWALL;
                 moveDelayTime = commonDelayTime;
                 break;
 
             case TURNDIAG:
-                // Move Turn turnDiagDegrees
-                moveTurn(45, turnSpeed);
+                moveTurn(45.0, turnSpeed);
                 currentMove = MoveState.STARTTURN;
                 nextMove = MoveState.MOVEDIAG;
                 telemetryMove = MoveState.TURNDIAG;
@@ -495,173 +453,49 @@ public class BEAST_MODE_Autonomous extends OpMode {
                 break;
 
             case MOVEDIAG:
-                // Move straight 70 + moveDiagDist to red
-//                lookingForWhiteLine = true; m
-                moveStraight(167 + moveDiagDist, mediumSpeed);
+                moveStraight(moveDiagDist, fastSpeed);
                 currentMove = MoveState.STARTMOVE;
-                nextMove = MoveState.TURNONCOLOREDLINE;
+                nextMove = MoveState.GOPASTREDTAPE;
                 telemetryMove = MoveState.MOVEDIAG;
                 moveDelayTime = commonDelayTime;
+                chosenOmni = OmniCtlr.EXTENDING;
                 break;
 
-            case TURNONCOLOREDLINE:
-                // Move Turn -45 degrees
-                moveTurn(45.0, turnSpeed);
-                currentMove = MoveState.STARTTURN;
-                nextMove = MoveState.DRIVETOBEACON;
-                telemetryMove = MoveState.TURNONCOLOREDLINE;
-                moveDelayTime = commonDelayTime;
-                break;
-
-//            case GOTOWHITELINE:
-//                 Move Straight to white line
-//                lookingForWhiteLine = true;
-//                moveStraight(45.0, slowSpeed);
-//                currentMove = MoveState.STARTMOVE;
-//                nextMove = MoveState.DONE;
-//                telemetryMove = MoveState.GOTOWHITELINE;
-//                moveDelayTime = commonDelayTime;
-//                break;
-
-//            case TURNTOBEACON:
-//                 Move Turn 90 degrees to the beacon
-//                moveTurn(90.0, turnSpeed);
-//                currentMove = MoveState.STARTTURN;
-//                nextMove = MoveState.DRIVETOBEACON;
-//                telemetryMove = MoveState.TURNTOBEACON;
-//                moveDelayTime = commonDelayTime;
-//                break;
-
-            case DRIVETOBEACON:
-                targetReading = 20.0;
-                lookingWithUltraSense = true;
-                moveStraight(50.0, mediumSpeed);
+            case GOPASTREDTAPE:
+                moveStraight(7.62, slowSpeed);
                 currentMove = MoveState.STARTMOVE;
-                nextMove = MoveState.EXTENDARM;
-                telemetryMove = MoveState.DRIVETOBEACON;
+                nextMove = MoveState.TURNPARALLELTOWALL;
+                telemetryMove = MoveState.GOPASTREDTAPE;
                 moveDelayTime = commonDelayTime;
                 break;
 
-//            case PUSHBUTTON:
-//                 push the button
-//                lookingWithUltraSense = false;
-//                nextMove = MoveState.UNPUSHBUTTON;
-//                telemetryMove = MoveState.PUSHBUTTON;
-//                break;
-//
-//            case UNPUSHBUTTON:
-//                 unpush the button
-//                nextMove = MoveState.BACKUP;
-//                telemetryMove = MoveState.UNPUSHBUTTON;
-//                break;
-
-//            case BACKUP:
-//                 Move Straight till can dump climbers
-//                lookingWithUltraSense = false;
-//                moveStraight(-5.0, mediumSpeed);
-//                currentMove = MoveState.STARTMOVE;
-//                nextMove = MoveState.EXTENDARM;
-//                telemetryMove = MoveState.BACKUP;
-//                moveDelayTime = commonDelayTime;
-//                break;
-
-            case EXTENDARM:
-                // Dump climbers
-                chinUp.setTargetPosition(-3000);
-                chinUp.setPower(0.5);
-                currentMove = MoveState.DELAYSETTINGS;
-                nextMove = MoveState.DUMPCLIMBERS;
-                telemetryMove = MoveState.EXTENDARM;
-                moveDelayTime = 500;
-                break;
-
-            case DUMPCLIMBERS:
-               if (!chinUp.isBusy()) {
-                   climberDumper.setPosition(1.0);
-                   currentMove = MoveState.DELAYSETTINGS;
-                   nextMove = MoveState.PULLARMIN;
-                   telemetryMove = MoveState.DUMPCLIMBERS;
-                   chosenOmni = OmniCtlr.EXTENDING;
-                   moveDelayTime = 1500;
-               }
-                break;
-
-            case PULLARMIN:
-                chinUp.setTargetPosition(-600);
-                chinUp.setPower(0.5);
-                currentMove = MoveState.DELAYSETTINGS;
-                nextMove = MoveState.TURNALONGLINE;
-                telemetryMove = MoveState.EXTENDARM;
-                moveDelayTime = 700;
-                break;
-
-
-            case TURNALONGLINE:
-                // Turn so we can position ourselves to go up the mountain
-                moveTurn(85.0, turnSpeed);
+            case TURNPARALLELTOWALL:
+                moveTurn(-45.0, turnSpeed);
                 currentMove = MoveState.STARTTURN;
-                nextMove = MoveState.BACKUPFARTHER;
-                telemetryMove = MoveState.TURNALONGLINE;
+                nextMove = MoveState.DRIVETOWHITELINE;
+                telemetryMove = MoveState.TURNPARALLELTOWALL;
                 moveDelayTime = commonDelayTime;
                 break;
 
-            case  BACKUPFARTHER:
-                // Move Straight 15 so we can drive to mountain
-                moveStraight(50.0, mediumSpeed);
+            case DRIVETOWHITELINE:
+                if (fColorSense.alpha() < 2.0) {
+                    leftDrive.setPower(0.2);
+                    rightDrive.setPower(0.2);
+                } else if (fColorSense.alpha() >= 2.0) {
+                    leftDrive.setPower(0.0);
+                    rightDrive.setPower(0.0);
+                }
                 currentMove = MoveState.STARTMOVE;
+                telemetryMove = MoveState.DRIVETOWHITELINE;
                 nextMove = MoveState.DONE;
-                telemetryMove = MoveState.BACKUPFARTHER;
-                moveDelayTime = commonDelayTime;
-                break;
-
-            case DRIVEALONGLINE:
-                // Move Straight 25 + diagMtnDist
-                moveStraight(25 + diagMtnDist, mediumSpeed);
-                currentMove = MoveState.STARTMOVE;
-                nextMove = MoveState.TURNTOMOUNTAIN;
-                telemetryMove = MoveState.DRIVEALONGLINE;
-                moveDelayTime = commonDelayTime;
-                break;
-
-            case TURNTOMOUNTAIN:
-                // Move Turn turnMtnDegrees
-                moveTurn(turnMtnDist, turnSpeed);
-                currentMove = MoveState.STARTTURN;
-                nextMove = MoveState.DRIVETOMOUNTAIN;
-                telemetryMove = MoveState.TURNTOMOUNTAIN;
-                moveDelayTime = commonDelayTime;
-                break;
-
-            case DRIVETOMOUNTAIN:
-                // Move Straight toMtnDist
-                moveStraight(toMtnDist, mediumSpeed);
-                currentMove = MoveState.STARTMOVE;
-                nextMove = MoveState.GOUPMOUNTAIN;
-                telemetryMove = MoveState.DRIVETOMOUNTAIN;
-                moveDelayTime = commonDelayTime;
-                break;
-
-            case GOUPMOUNTAIN:
-                // Move Straight up mountain
-                moveStraight(100.0, mediumSpeed);
-                currentMove = MoveState.STARTMOVE;
-                nextMove = MoveState.PREPTELEOP;
-                telemetryMove = MoveState.GOUPMOUNTAIN;
-                moveDelayTime = commonDelayTime;
-                break;
-
-            case PREPTELEOP:
-                // get robot ready for TeleOp
-                nextMove = MoveState.DONE;
-                telemetryMove = MoveState.PREPTELEOP;
                 moveDelayTime = commonDelayTime;
                 break;
 
             case DONE:
                 leftDrive.setPower(0.0);
                 rightDrive.setPower(0.0);
-                endGameLights.setPower(0.5);
-                break;
+                telemetryMove = MoveState.DONE;
+
         }
 
         switch (currentOmni) {
